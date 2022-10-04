65d5e01f4e03b7372a928222e46027dfb4001369
pavelsurovy <surolm@centrum.sk>
2022-10-04 18:54:06 +0200

    funkcny picker

--- Kviziq/PickerController.swift
+++ Kviziq/PickerController.swift
@@ -7,10 +7,11 @@
 
 import UIKit
 
-class PickerController: UIViewController {
+class PickerController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
     @IBOutlet weak var kvizPicker: UIPickerView!
     var kvizArray = [String]()
+    var vybratyKviz = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,7 +23,27 @@ class PickerController: UIViewController {
             kvizArray.append(item)
         }
         
-        print(kvizArray)
+        kvizArray.sort()
+        
+        kvizPicker.dataSource = self
+        kvizPicker.delegate = self
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return kvizArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let rozdel = kvizArray[row].components(separatedBy: ".")
+        return rozdel[0]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        vybratyKviz = kvizArray[row]
     }
 }
 
